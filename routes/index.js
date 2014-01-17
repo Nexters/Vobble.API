0eddf745dba26793e2989f345bee9b6aa17e5aa0
swlee <sw@glytter.me>
2014-01-17 19:59:26 +0900

    object 코딩 스타일 변경

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,10 @@ var handlers = exports.handlers = {
     User.find({ where: { email: email } }).success(function(user) {
       if (user) {
         // TODO: 상태 코드 맞는지 확인
-        res.send(400, { result: 0, msg: '이미 가입된 회원'});
+        res.send(400, {
+          result: 0,
+          msg: '이미 가입된 회원'
+        });
       } else {
         var token = crypto
                       .createHash('md5')
@@ -47,13 +50,22 @@ var handlers = exports.handlers = {
         };
 
         User.create(user).success(function() {
-          res.send(200, { result: 1, msg: '회원 가입 성공' });
+          res.send(200, {
+            result: 1,
+            msg: '회원 가입 성공'
+          });
         }).error(function(err) {
-          res.send(500, { result: 0, msg: '서버 오류' });
+          res.send(500, {
+            result: 0,
+            msg: '서버 오류'
+          });
         });
       }
     }).error(function(err) {
-      res.send(500, { result: 0, msg: '서버 오류' });
+      res.send(500, {
+        result: 0,
+        msg: '서버 오류'
+      });
     });
   },
 
@@ -65,12 +77,27 @@ var handlers = exports.handlers = {
 
     User.find({ where: { email: email, password: password } }).success(function(user) {
       if (user) {
-        res.send(200, { result: 1, msg: '로그인 성공', user_id: user.user_id, token: user.token });
+        res.send(200, {
+          result: 1,
+          msg: '로그인 성공',
+          user_id: user.user_id,
+          token: user.token
+        });
       } else {
-        res.send(400, { result: 0, msg: '회원 정보 없음', user_id: -1, token: -1 });
+        res.send(400, {
+          result: 0,
+          msg: '회원 정보 없음',
+          user_id: -1,
+          token: -1
+        });
       }
     }).error(function(err) {
-      res.send(500, { result: 0, msg: '서버 오류', user_id: -1, token: -1 });
+      res.send(500, {
+        result: 0,
+        msg: '서버 오류',
+        user_id: -1,
+        token: -1
+      });
     });
   }
 };
