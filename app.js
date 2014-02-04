11b52ce38c20c8dfd2161befadbb108b8dd92396
blaswan <lsw9549@gmail.com>
2014-02-04 17:40:02 +0900

    heroku 배포 환경 설정

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 'use strict';
 
 var express = require('express')
-  , app = express();
+  , app = express()
+  , url = require('url');
 
 
 /* development 환경 설정 */
 if (app.get('env') === 'development') {
   app.set('config', require('./config/development.json'));
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.limit('5mb'));
+  app.use(express.bodyParser({uploadDir: __dirname + '/files'}));
+  app.use(express.methodOverride());
+  app.use(express.logger({ buffer: 5000}));
+  app.use(app.router);
+  app.use(express.errorHandler());
+}
+
+/* test 환경 설정 - heroku */
+if (app.get('env') === 'test') {
+  var dbUrl = url.parse(process.env.CLEARDB_DATABASE_URL);
+  var testConfig = {
+    "db": {
+      "database": dbUrl.path.substr(1).split('?')[0],
+      "username": dbUrl.auth.split(':')[0],
+      "password": dbUrl.auth.split(':')[1],
+      "options": {
+        "host": dbUrl.hostname,
+        "port": dbUrl.port,
+        "logging": false,
+        "sync": { "force": false },
+        "define": {
+          "charset": "utf8",
+          "underscored": true,
+          "timestamps": true
+        }
+      }
+    }
+  };
+  app.set('config', testConfig);
   app.set('port', process.env.PORT || 3000);
   app.use(express.limit('5mb'));
   app.use(express.bodyParser({uploadDir: __dirname + '/files'}));
@@ -36,4 +68,8 @@ require('./routes').init(app);
 /* 서버 실행 */
 app.listen(app.get('port'), function() {
   console.log('Hello, world!');
+});
+
+process.on('uncaughtException', function(err) {
+  console.error(err.stack);
 });
